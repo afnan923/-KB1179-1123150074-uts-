341e3494ced48a6f0526baf8a4ae0936d700d73f
afnan923 <susumurni013@gmail.com>
2025-11-07 11:50:42 +0700

    add social button

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -112,7 +112,15 @@ class Login extends StatelessWidget {
                             ],
                             
                           ),
-              ]
+                          OutlinedButton.icon(onPressed: (){},
+                          icon: Icon(Icons.g_mobiledata, color: Colors.grey[800]),
+                          label: Text('Login dengan Google', style: TextStyle(color: Colors.grey),),
+                          style: OutlinedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(20),
+                            side: BorderSide(color: Colors.grey[300]!),)
+                          ),)
+              ],
             ),
           ),
         ),
