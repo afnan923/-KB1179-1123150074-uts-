a692cef9900c55015160fefb55f47db2bb9ac484
afnan923 <susumurni013@gmail.com>
2025-11-07 11:48:38 +0700

    add border email and password

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -25,6 +25,53 @@ class Login extends StatelessWidget {
                   "Silahkan Login untuk melanjutkan",
                   style: TextStyle(fontSize: 16.0, color: Colors.green),
                 ),
+                const SizedBox(height: 10),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Masukan Email Anda",
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: Colors.blue.shade700,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Masukan Password",
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.visibility_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: Colors.blue.shade700,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
               ]
             ),
           ),
