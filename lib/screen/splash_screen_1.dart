a2dd71b0aef233e3c6ac4fd7e11f3fceacecd5b2
afnan923 <susumurni013@gmail.com>
2025-11-07 11:29:59 +0700

    add button

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -71,6 +71,26 @@ class MySplashScreen extends StatelessWidget {
               ),
               ],
             ),
+              SizedBox(height: 20,),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40),
+              child: SizedBox(
+                height: 25,
+                width: double.infinity,
+                child: ElevatedButton(onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, 
+                ), 
+                child: Text("Continue",
+                style: TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+              ),
+              ),
+              ),
+              ),
+            ),
+            SizedBox(height: 30,)
           ],
         ),
       ),
